e9c8b7682741dfc1c2be3b9321f63a9fc9ef8ec7
og8217 <og8217@intl.att.com>
2023-01-15 16:43:12 +0200

    add & use constants_metadata.TOPIC_LISTENER_OPTIONS_METADATA_KEY

--- libs/message-broker/src/decorators/topic-listener.decorator.ts
+++ libs/message-broker/src/decorators/topic-listener.decorator.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata";
 import { Injectable, InjectableOptions } from "@nestjs/common"
+import { constants_metadata } from "../constants";
 
 
 export function TopicListenerInjectable(
     listenerOptions: { connectionToken: string },
     options?: InjectableOptions) : ClassDecorator {
     return function(constructor: Function) {
-        Reflect.defineMetadata('topic-listener-options', listenerOptions, constructor);
+        Reflect.defineMetadata(
+            constants_metadata.TOPIC_LISTENER_OPTIONS_METADATA_KEY, 
+            listenerOptions, constructor);
         Injectable(options)(constructor);
     };
 }
